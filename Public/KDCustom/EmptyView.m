030ed51ce3c3a67da0c3ab6bac23a7bebb802eb2
wbzhan <zhanwenbaoit@126.com>
2018-05-24 15:56:59 +0800

    无数据页面去除刷新按钮

--- Public/KDCustom/EmptyView.m
+++ Public/KDCustom/EmptyView.m
@@ -13,32 +13,36 @@
 //重写view
 - (void)prepare{
     [super prepare];
-//主标题自定义
+    //主标题自定义
     self.self.titleLabFont = SYSTEMSIZE(28);
     self.self.titleLabTextColor = RGB_153;
-//按钮自定义
-    self.actionBtnBorderColor = RGB_153;
-    self.actionBtnTitleColor = RGB_153;
-    self.actionBtnFont = SYSTEMSIZE(30);
-    self.actionBtnBorderWidth = kLineHeight;
-//图片大小自定义
+    //按钮自定义
+    //    self.actionBtnBorderColor = RGB_153;
+    //    self.actionBtnTitleColor = RGB_153;
+    //    self.actionBtnFont = SYSTEMSIZE(30);
+    //    self.actionBtnBorderWidth = kLineHeight;
+    //图片大小自定义
     self.imageSize = CGSizeMake(kSizeFrom750(330), kSizeFrom750(330));
-
+    
     self.autoShowEmptyView = NO;//改为手动控制是否显示
 }
 +(instancetype)noDataEmpty{
-
-    return [EmptyView emptyViewWithImageStr:@"nodata"
-                                    titleStr:@"暂无数据"
-                                   detailStr:@"请检查您的网络连接是否正确!"];
+    
+    return [EmptyView emptyViewWithImageStr:@"icons_nodata"
+                                   titleStr:@"暂无数据"
+                                  detailStr:@"请检查您的网络连接是否正确!"];
 }
 
 //带刷新的nodataView
 +(instancetype)noDataRefreshBlock:(EmptyRefreshBlock)refreshBlock
 {
-  return [EmptyView emptyActionViewWithImageStr:@"icons_nodata" titleStr:@"暂无数据" detailStr:@"" btnTitleStr:@"重新加载" btnClickBlock:^{
-        refreshBlock();
-    }];
-  }
+    //  return [EmptyView emptyActionViewWithImageStr:@"icons_nodata" titleStr:@"暂无数据" detailStr:@"" btnTitleStr:@"" btnClickBlock:^{
+    //        refreshBlock();
+    //    }];
+    return [EmptyView emptyViewWithImageStr:@"icons_nodata"
+                                   titleStr:@"暂无数据"
+                                  detailStr:@""];
+}
+
 
 @end
