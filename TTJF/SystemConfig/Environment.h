56eb528a68a9632d7229452c031a12b477d1e062
wbzhan <zhanwenbaoit@126.com>
2018-07-30 11:17:13 +0800

    正式环境、开发环境一键切换

--- TTJF/SystemConfig/Environment.h
+++ TTJF/SystemConfig/Environment.h
@@ -9,10 +9,11 @@
 #ifndef Environment_h
 #define Environment_h
 
-#define App_Environment 2 //1.正式环境 2.测试环境
+//#define App_Environment 2 //1.正式环境 2.测试环境
 //webView页面跳转域名校验
 #define urlCheckAddress @"tutujf.com"
-#if(App_Environment==1)
+//#if(App_Environment==1)
+#ifdef DEBUG//正式环境
 //app接口域名
 #define oyApiUrl            @"https://api.tutujf.com"
 //图片前置地址
@@ -24,7 +25,8 @@
 //系统加密所需要的系统token
 #define systemToken @"!Y9v9OK41w(2"
 
-#elif(App_Environment==2)//测试环境
+//#elif(App_Environment==2)//测试环境
+#elif TTJF_DEV//开发测试环境
 //app接口域名
 #define oyApiUrl             @"https://cs.api.tutujf.com"
 //图片前置地址
